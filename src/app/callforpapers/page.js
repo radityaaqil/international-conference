b57cba5b7c902273f5ccbcd438c3596761b9a66c
Raditya Aqil <raditya.aqil@efishery.com>
2024-01-20 22:06:34 +0700

    add link easychair and add poster

--- src/app/callforpapers/page.js
+++ src/app/callforpapers/page.js
@@ -17,13 +17,16 @@ const Callforpapers = () => {
                 <Navbar pathname={pathname}/>
                 <div className="border-b-2 border-gray-300 text-white mx-52">a</div>
                 <div className="mx-52 mt-8">
-                <div className="w-full h-[400px] bg-yellow-500 text-7xl font-bold text-white">INSERT POSTER HERE</div>
+                {/* <div className="w-full h-[400px] bg-yellow-500 text-7xl font-bold text-white">INSERT POSTER HERE</div> */}
+                <div className="flex justify-center">
+                    <img src="./poster.jpeg" className="h-[700px]"/>
+                </div>
                 {/* <div className="text-xl font-bold mb-6">Submit Paper</div> */}
                 {/* <div className="text-justify text-xl">
                 The iconSET-2024 is a scientific conference organized by experience scientific committee and organizer. During  the conference researcher, student, academic, and industrial practitioners discuss the achievement in the field of engineering and science for sustainable development. In the 1st iconSET 2024, the conference focus on the science and engineering for energy technology for green energy solution. This year conference is co-organized by Centre for materials and nanotechnology ITS and ITS Tekno Sains Academy. 
                 </div> */}
-                <div className="text-xl font-bold my-6 font-montserrat">Subjects and Topics</div>
-                <div className="flex items-center text-lg space-x-4 font-montserrat">
+                <div className="text-xl font-bold mt-10 font-montserrat">Subjects and Topics</div>
+                <div className="flex items-center text-lg space-x-4 font-montserrat mt-4">
                         <GoDotFill className="text-sm text-[#df0201]"/>
                         <div>Mechanical, material, and chemical science and engineering for energy application</div>
                     </div>
@@ -55,15 +58,17 @@ const Callforpapers = () => {
           <Header pathname={pathname}/>
           <div className="mt-8 mx-12 font-montserrat">
             <div className="text-lg font-bold mb-4">Call for Papers</div>
-          <div className="w-full h-[400px] bg-yellow-500 text-xl font-bold text-white">INSERT POSTER HERE</div>
+            <div className="flex justify-center">
+                    <img src="./poster.jpeg" className="h-[500px]"/>
+                </div>
                 {/* <div className="text-xl font-bold mb-6">Submit Paper</div> */}
                 {/* <div className="text-justify text-xl">
                 The iconSET-2024 is a scientific conference organized by experience scientific committee and organizer. During  the conference researcher, student, academic, and industrial practitioners discuss the achievement in the field of engineering and science for sustainable development. In the 1st iconSET 2024, the conference focus on the science and engineering for energy technology for green energy solution. This year conference is co-organized by Centre for materials and nanotechnology ITS and ITS Tekno Sains Academy. 
                 </div> */}
                 <div className="flex justify-center">
                     <div className="font-montserrat"> 
-                        <div className="text-lg font-bold my-6">Subjects and Topics</div>
-                        <div className="flex items-center text-base gap-2">
+                        <div className="text-lg font-bold mt-6">Subjects and Topics</div>
+                        <div className="flex items-center text-base gap-2 mt-4">
                             <GoDotFill className="text-sm text-[#df0201] w-[25px]"/>
                             <div className="w-[300px]">Mechanical, material, and chemical science and engineering for energy application</div>
                         </div>
